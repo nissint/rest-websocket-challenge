8123c9daec34c2bb832d3f66bf0513ce0e123e5c
qtpeter <quinn.peters@nissint.com>
2015-11-13 17:23:03 -0500

    Got the REST stuff working

--- src/main/java/com/nissint/example/ExampleResource.java
+++ src/main/java/com/nissint/example/ExampleResource.java
@@ -1,5 +1,17 @@
 package com.nissint.example;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("example")
 public class ExampleResource {
 
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getCities() {
+		
+		return "Stuff\n";
+	}
 }
